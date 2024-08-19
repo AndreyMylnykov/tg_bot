2a1b8de96aa95bd9904a4d57a60744aa76b56817
AndreyMylnykov <116729572+AndreyMylnykov@users.noreply.github.com>
2024-08-19 23:46:03 +0300

    Add files via upload

--- createBot.py
+++ createBot.py
@@ -6,16 +6,26 @@ import numpy as np
 import time
 import warnings
 
+# Initialize colorama for colored terminal output
 init(autoreset=True)
+
+# Define custom color function for terminal output
 def custom_color(r, g, b):
     return f'\033[38;2;{r};{g};{b}m'
+
+# Define custom colors
 grey_color = custom_color(80, 80, 80)
+green_color = custom_color(47, 201, 0)
+
+# Initialize global filenames
 json_filename = ""
 npm_filename = ""
 
+# Extract the first string from each sub-array
 def extract_first_strings(two_dim_array):
     return [pair[0] for pair in two_dim_array if pair[0]]
 
+# Get a valid number input from the user
 def get_number(to, text):
     result = 0
     success = False
@@ -23,51 +33,61 @@ def get_number(to, text):
         a = input(text)
         try:
             result = int(a)
-            if result >= 0 and result <= to:
+            if 0 <= result <= to:
                 success = True
             else:
                 print(Fore.RED + f"Please enter a value between 0 and {to}.")
-        except:
-            print(Fore.RED + f"Please enter a valid number between 0 and {to}..")
+        except ValueError:
+            print(Fore.RED + f"Please enter a valid number between 0 and {to}.")
     return result
 
+# Precompute embeddings and save them to a file
 def precompute_embeddings(array_of_phrases, batch_size=1000, filename='precomputed_embeddings.npy'):
     global npm_filename
     npm_filename = filename
-    print("Loading model...", grey_color + "(it may take some time, plesae wait)")
+    print("Loading model...", grey_color + "(it may take some time, please wait)")
     start_time = time.time()
 
+    # Import the necessary modules only when needed
     from sentence_transformers import SentenceTransformer
     warnings.filterwarnings("ignore", message=".*clean_up_tokenization_spaces*.")
+
+    # Load the model
     model = SentenceTransformer('paraphrase-multilingual-MiniLM-L12-v2')
-    print(Fore.GREEN + f"Model loaded successfully in {round(time.time() - start_time,1)}s\n")
+    print(Fore.GREEN + f"Model loaded successfully in {round(time.time() - start_time, 1)}s\n")
 
     print("Precomputing embeddings...", grey_color + "(this can also take a few minutes)")
     phrase_vectors = []
+
+    # Compute embeddings in batches
     for i in range(0, len(array_of_phrases), batch_size):
         batch_phrases = array_of_phrases[i:i + batch_size]
         batch_vectors = model.encode(batch_phrases)
         phrase_vectors.append(batch_vectors)
+
+    # Stack and save embeddings
     phrase_vectors = np.vstack(phrase_vectors).astype('float32')
-    np.save(filename, phrase_vectors)  # Save embeddings for later use
+    np.save(filename, phrase_vectors)
     print(green_color + "Successfully saved embeddings to", Fore.GREEN + filename)
     return phrase_vectors
 
+# Convert message text to a string
 def convert_text_to_string(text):
     if isinstance(text, list):
         return "".join(part["text"] if isinstance(part, dict) and "text" in part else str(part) for part in text)
     return text
 
+# Process chat data to extract messages
 def process_chat(data):
     print("\nFound", green_color + str(len(data["messages"])), "messages.\n")
     message_dict = {}
     message_array = []
     people = []
-    
-    
+
+    # Iterate through messages and build a dictionary
     for message in data["messages"]:
         if message["type"] == "message" and "text" in message:
-            if not message["from"] in people:
+            if message["from"] not in people:
                 people.append(message["from"])
             message_id = message.get("id")
             if message_id:
@@ -76,89 +96,92 @@ def process_chat(data):
                     "text": convert_text_to_string(message["text"]),
                     "reply_to_message_id": message.get("reply_to_message_id")
                 }
+
+    # Prompt user to choose a bot persona
     print("Choose who you want to make a bot out of:")
     for number, name in enumerate(people):
-        print(" ", str(number),"- ",Fore.CYAN + name)
+        print(" ", str(number), "- ", Fore.CYAN + name)
 
-    number = get_number(1, "\nEnter the corresponding number: ")
+    number = get_number(len(people) - 1, "\nEnter the corresponding number: ")
 
+    # Create an array of message pairs
     for message_id, message in message_dict.items():
         if message["reply_to_message_id"]:
             reply_message = message_dict.get(message["reply_to_message_id"])
             if reply_message:
-                # Check if the reply message is not from you and neither message is empty
                 if reply_message["from"] != people[number] and reply_message["text"].strip() and message["text"].strip() and message["from"] == people[number]:
-                    # Add the original message and its reply to the array
                     message_array.append([reply_message["text"], message["text"]])
 
     print(green_color + f"Successfully extracted {len(message_array)} messages from replies!")
     return message_array
 
-def saveChatArray(array, filename = 'processed_chat.json'):
+# Save the processed chat array to a file
+def save_chat_array(array, filename='processed_chat.json'):
     global json_filename
     json_filename = filename
     with open(filename, 'w', encoding='utf-8') as f:
         json.dump(array, f, ensure_ascii=False, indent=4)
     print(green_color + "Successfully saved chat array to", Fore.GREEN + filename)
 
-def loadChatArray(filename):
+# Load a chat array from a file
+def load_chat_array(filename):
     with open(filename, 'r', encoding='utf-8') as f:
         return json.load(f)
 
 try:
-    green_color = custom_color(47, 201, 0)
+    print(Fore.GREEN + "Welcome to createBot.py!", "\nThe goal of this script is to create a", Fore.CYAN + ".npy", "file and a", Fore.CYAN + ".json", "file required for the bot to work.\nFor this, the script requires a", Fore.CYAN + ".json", "file which you can get from Telegram's", Fore.BLUE + '"Export chat history"', "option.\n")
 
-    print(Fore.GREEN + "Welcome to createBot.py!","\nThe goal of this script is to create a",Fore.CYAN + ".npy", "file and a",Fore.CYAN+".json","files that are required for the bot to work.\nFor this, the script requires a",Fore.CYAN + ".json","file which you can get from Telegram's",Fore.BLUE + '"Export chat history"', "option.\n")
-
+    # Prompt the user to name the bot
     success = False
-    name = ''
     while not success:
         name = input("\nChoose a name for the bot: ")
-        if name != '':
-            os.makedirs(name, exist_ok = True)
+        if name:
+            os.makedirs(name, exist_ok=True)
             success = True
 
-               
-    with open(name + "/name.txt", 'w') as file:
+    # Save the bot name
+    with open(os.path.join(name, "name.txt"), 'w') as file:
         file.write(name)
-    name += "/" 
+    name = os.path.join(name, "")
 
+    # Search for JSON files in the current directory
     script_dir = os.path.dirname(os.path.abspath(__file__))
     json_files = glob.glob(os.path.join(script_dir, '*.json'))
-
-    num_files = len(json_files)
+    
     val = 0
-    if num_files == 0:
-        print(Fore.RED + "No JSON files found in the directory.\n", "Please make sure to put a",Fore.CYAN + ".json","file in the same directory as this script.")
+    if not json_files:
+        print(Fore.RED + "No JSON files found in the directory.\nPlease make sure to put a", Fore.CYAN + ".json", "file in the same directory as this script.")
         input("Press ENTER to exit...")
         quit()
     else:
-        print("Found", green_color + str(num_files), ".json files in the directory.")
-        if num_files == 1:
+        print("Found", green_color + str(len(json_files)), ".json files in the directory.")
 
-            for json_file in json_files:
-                print(green_color + f"JSON file: {os.path.basename(json_file)}")
-                input("\nPress ENTER if correct...")
+        # If only one JSON file is found, confirm with the user
+        if len(json_files) == 1:
+            print(green_color + f"JSON file: {os.path.basename(json_files[0])}")
+            input("\nPress ENTER if correct...")
         else:
+            # List all found JSON files and let the user choose
             for number, json_file in enumerate(json_files):
-                print(" ", str(number),"- ",Fore.CYAN + str(os.path.basename(json_file)))
+                print(" ", str(number), "- ", Fore.CYAN + os.path.basename(json_file))
 
-            #HERE!
-            val = get_number(num_files-1, "\nEnter the corresponding number for a" + Fore.CYAN + ".json"+Fore.WHITE +" file that you exported from Telegram:")
+            val = get_number(len(json_files) - 1, "\nEnter the corresponding number for a" + Fore.CYAN + ".json" + Fore.WHITE + " file that you exported from Telegram:")
             print("Processing chat...")
 
+    # Load the selected JSON file
     filename = json_files[val]
-
-
     with open(filename, 'r', encoding='utf-8') as f:
         data = json.load(f)
 
+    # Process the chat data
     message_array = process_chat(data)
-    saveChatArray(message_array, filename = name + 'processed_chat.json')
 
-    precompute_embeddings(extract_first_strings(message_array), filename = name + 'precomputed_embeddings.npy')
+    # Save processed chat and precompute embeddings
+    save_chat_array(message_array, filename=name + 'processed_chat.json')
+    precompute_embeddings(extract_first_strings(message_array), filename=name + 'precomputed_embeddings.npy')
 
-    token = input("\n\nEnter the bot's token from BotFather"+grey_color+" (you can change it later in a file token.txt)"+Fore.WHITE+": ")
+    # Prompt the user for the bot's token and save it
+    token = input("\n\nEnter the bot's token from BotFather" + grey_color + " (you can change it later in a file token.txt)" + Fore.WHITE + ": ")
     with open(name + "token.txt", 'w') as file:
         file.write(token)
 
--- launchBot.py
+++ launchBot.py
@@ -1,51 +1,58 @@
 import time
-from colorama import Fore, Style, init
+from colorama import Fore, init
 init(autoreset=True)
+
+# Function to generate a custom color
 def custom_color(r, g, b):
     return f'\033[38;2;{r};{g};{b}m'
+
+# Define colors
 grey_color = custom_color(80, 80, 80)
 green_color = custom_color(47, 201, 0)
 
 import os
+
+# Function to get a valid number input from the user
 def get_number(to, text):
-    result = 0
-    success = False
-    while not success:
+    while True:
         a = input(text)
         try:
             result = int(a)
-            if result >= 0 and result <= to:
-                success = True
+            if 0 <= result <= to:
+                return result
             else:
                 print(Fore.RED + f"Please enter a value between 0 and {to}.")
-        except:
-            print(Fore.RED + f"Please enter a valid number between 0 and {to}..")
-    return result
+        except ValueError:
+            print(Fore.RED + f"Please enter a valid number between 0 and {to}.")
 
+# Get the current script's directory
 script_directory = os.path.dirname(os.path.abspath(__file__))
 
+# Get a list of bot folders that contain a name.txt file
 bot_folders = [
     name for name in os.listdir(script_directory)
     if os.path.isdir(os.path.join(script_directory, name)) and
     os.path.isfile(os.path.join(script_directory, name, 'name.txt'))
 ]
 
+print(Fore.GREEN + "Welcome to launchBot.py!")
+print("\nThis program launches a Telegram bot based on the chat history. For this bot to work, you'll need 3 things:")
+print("1. processed_chat.json")
+print("2. precomputed_embeddings.npy")
+print("3. Bot token\n")
+print("To get the first two, you'll need to first launch the createBot.py script. For the third, just google it.\n")
 
-number = 0
-print(Fore.GREEN + "Welcome to launchBot.py!", "\nThis program launches a Telegram bot based on the chat history. For this bot to work, you'll need 3 things:\n1. processed_chat.json\n2. precomputed_embeddings.npy\n3. Bot token\n\nTo get the first two, you'll need to first launch the createBot.py script. For the third, just google it.\n")
+# Handle cases based on the number of bot folders available
 if len(bot_folders) == 0:
     input("There are no bot folders in the script's directory. Please visit createBot.py...")
     exit()
 elif len(bot_folders) == 1:
-    input("Press ENTER to launch: "+ Fore.CYAN + bot_folders[0])
+    input("Press ENTER to launch: " + Fore.CYAN + bot_folders[0])
 else:
     print("Choose which bot to launch:")
     for number, name in enumerate(bot_folders):
-        print(" ", str(number),"-",Fore.CYAN + name)
-    number = get_number(len(bot_folders)-1, "\nEnter the corresponding number: ")
-
-print(bot_folders[number])
-
+        print(" ", str(number), "-", Fore.CYAN + name)
+    number = get_number(len(bot_folders) - 1, "\nEnter the corresponding number: ")
 
 print(Fore.CYAN + "Initializing modules...")
 start_time = time.time()
@@ -57,19 +64,20 @@ import faiss
 import warnings
 import random
 import re
+
+# Suppress specific warnings
 warnings.filterwarnings("ignore", category=FutureWarning, module="transformers")
 
-
-
+# Load the SentenceTransformer model
 model = SentenceTransformer('paraphrase-multilingual-MiniLM-L12-v2')
-print(Fore.GREEN + f"Initialized all modules in {round(time.time() - start_time,1)}s\n")
+print(Fore.GREEN + f"Initialized all modules in {round(time.time() - start_time, 1)}s\n")
 
-
+# Function to load chat array from a JSON file
 def load_chat_array(filename):
     with open(filename, 'r', encoding='utf-8') as f:
         return json.load(f)
 
-# New function to precompute and save embeddings
+# Function to precompute and save embeddings
 def precompute_embeddings(array_of_phrases, batch_size=1000, filename='precomputed_embeddings.npy'):
     print("Precomputing embeddings...")
     phrase_vectors = []
@@ -81,76 +89,62 @@ def precompute_embeddings(array_of_phrases, batch_size=1000, filename='precomput
     np.save(filename, phrase_vectors)  # Save embeddings for later use
     return phrase_vectors
 
+# Function to load precomputed embeddings from a file
 def load_embeddings(filename):
     return np.load(filename)
 
-
-
+# Function to find similar phrases using FAISS
 def find_similar(phrase, embeddings, k=5):
-    # Encode the input phrase
     phrase_vector = model.encode([phrase]).astype('float32')
-
-    # Create a FAISS index and add embeddings to it
     index = faiss.IndexFlatL2(embeddings.shape[1])
     index.add(embeddings)
-
-    # Search for the k nearest neighbors
     D, I = index.search(phrase_vector, k)
-
-    # Randomly select one of the k nearest neighbors
     selected_index = np.random.choice(I[0])
     return selected_index
-    
 
+# Function to extract the first element from each pair in a 2D array
 def extract_first_strings(two_dim_array):
     return [pair[0] for pair in two_dim_array if pair[0]]
 
-
+# Function to get an answer based on the input text
 def get_answer(text, embeddings, message_array):
     index = find_similar(text, embeddings)
     my_message, his_message = loaded_message_array[index]
-    #print(grey_color + f"({my_message})")
     return his_message
 
-
-loaded_message_array = load_chat_array(bot_folders[number]+"/"+'processed_chat.json')
+# Load the chat array and embeddings
+loaded_message_array = load_chat_array(bot_folders[number] + "/" + 'processed_chat.json')
 inputs = extract_first_strings(loaded_message_array)
-
-#embeddings = precompute_embeddings(inputs)
-embeddings = load_embeddings(bot_folders[number]+"/"+'precomputed_embeddings.npy')
-
+embeddings = load_embeddings(bot_folders[number] + "/" + 'precomputed_embeddings.npy')
 
 '''
                TELEGRAM PART
 '''
 
-
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
+# Function to get a response from the bot
 def get_response(input_text):
     return get_answer(input_text, embeddings, loaded_message_array)
 
+# Handler for the /start command
 def start(update, context):
     global name
-    update.message.reply_text("Hello, I'm "+name+". Ask me anything!")
+    update.message.reply_text("Hello, I'm " + name + ". Ask me anything!")
 
+# Handler for incoming messages
 def handle_message(update, context):
     global name
     user_input = update.message.text
-
     chat_type = update.message.chat.type  # Get the type of chat
     answering = False
 
-
     if update.message.reply_to_message and update.message.reply_to_message.from_user.id == context.bot.id:
         answering = True
-    elif chat_type == 'group' or chat_type == 'supergroup':
+    elif chat_type in ['group', 'supergroup']:
         chance = 0.2
         if random.random() < chance or name.lower() in user_input.lower():
             answering = True
-            #user_input = re.sub(r'свят', '', user_input, flags=re.IGNORECASE)
-            #print(user_input)
-
     elif chat_type == 'private':
         answering = True
 
@@ -158,52 +152,46 @@ def handle_message(update, context):
         response = get_response(user_input)
         print(f"{update.message.from_user.full_name}: {user_input}")
         print(f"Bot Response: {response}\n")
-        
         update.message.reply_text(response)
     else:
         print("ignoring")
 
 name = ''
+
+# Main function to start the bot
 def main():
     global name
-    filename = bot_folders[number]+ "/token.txt"
+    filename = bot_folders[number] + "/token.txt"
     TOKEN = ''
 
     if os.path.isfile(filename):
         with open(filename, 'r') as file:
             TOKEN = file.read().strip()
-        #print(f"Token read from file: {TOKEN}")
     else:
         TOKEN = input("Enter your Bot token from BotFather: ")
-        
         with open(filename, 'w') as file:
             file.write(TOKEN)
         print(f"Token saved to {filename}")
 
-    filename =bot_folders[number]+ "/name.txt"
+    filename = bot_folders[number] + "/name.txt"
 
     if os.path.isfile(filename):
         with open(filename, 'r') as file:
             name = file.read().strip()
     else:
         name = input("Enter your Bot's name: ")
-        
         with open(filename, 'w') as file:
             file.write(name)
         print(f"Name saved to {filename}")
 
     updater = Updater(TOKEN, use_context=True)
-
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))
+
     print("The bot is running.")
-
-    # Start the bot
     updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT, SIGTERM or SIGABRT
     updater.idle()
 
 if __name__ == '__main__':
